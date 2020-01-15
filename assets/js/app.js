6c425a81a5b393d085c16f6437e3572530bab576
leoboyerbx <leoboyer.bx@gmail.com>
2020-01-15 22:04:23 +0100

    Added footer base and fixed sticky circuit bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,18 +6,43 @@ import createMenu from "@/modules/menu";
 import equipmentSearch from "@/components/equipmentSearch";
 
 $(document).ready(function() {
+    //config footer
+    function footerHeight () {
+        $('.page--content').css('margin-bottom', $('#main-footer').outerHeight())
+    }
+    footerHeight()
+    window.onresize = footerHeight
+
     if (document.body.dataset.page === '1') {
         // Si on est sur la page geii
         const $circuit = $('#circuit')
-        const circuitOffsetTop = $circuit.offset().top
+        let circuitOffsetTop
+        let circuitOffsetBottom
+        const updateCircuitOffsetTop = function () {
+            circuitOffsetTop = $('#intro').offset().top
+        }
+        const updateCircuitOffsetBottom = function () {
+            circuitOffsetBottom = $('#plateforme__page-content').offset().top + $('#plateforme__page-content').innerHeight() - $circuit.outerHeight()
+        }
+        updateCircuitOffsetTop()
+        updateCircuitOffsetBottom()
+        console.log(circuitOffsetBottom)
+        $circuit.css('top', circuitOffsetTop)
+
         const checkStickyCircuit = function () {
-            if (window.scrollY >= circuitOffsetTop) {
+            if (window.scrollY >= circuitOffsetTop && window.scrollY < circuitOffsetBottom) {
                 $circuit.addClass('sticky')
+                $circuit.css('top', 0)
+            } else if (window.scrollY >= circuitOffsetBottom) {
+                $circuit.removeClass('sticky')
+                $circuit.css('top', circuitOffsetBottom)
             } else {
                 $circuit.removeClass('sticky')
+                $circuit.css('top', circuitOffsetTop)
             }
         }
         $(document).scroll(checkStickyCircuit)
+        $(window).resize(function () { updateCircuitOffsetBottom(); updateCircuitOffsetTop() })
 
         const spy = new ScrollSpy($('.slide'), 0.7)
         const circuitClass = new AssignClass($circuit, 'scroll-')
